d5d47782a353dfd16adde138f99d5192dd595eb0
dalgonaio <jungmeevlee@gmail.com>
2023-10-17 17:11:59 -0400

    add async handler from express

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,19 @@
 import {Request, Response} from 'express';
+import asyncHandler from 'express-async-handler';
 
 //@desc Get all users
 //@route GET /users
 //@access public < Jungmee change this once you add authentication
 
-export const getUsers = (req: Request, res: Response) => {
+export const getUsers = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({message: 'Get all users, love!'});
-};
+});
 
 //@desc create 1 new user
 //@route POST /users/
 //@access public < Jungmee change this once you add authentication
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = asyncHandler(async (req: Request, res: Response) => {
   const {firstName, lastName, email} = req.body;
 
   if (!firstName || !lastName || !email) {
@@ -21,13 +22,13 @@ export const createUser = (req: Request, res: Response) => {
   }
 
   res.status(201).json({message: 'Love, you created a new user!'});
-};
+});
 
 //@desc get 1 user
 //@route GET /users/:id
 //@access public < Jungmee change this once you add authentication
 
-export const getSingleUser = (req: Request, res: Response) => {
+export const getSingleUser = async (req: Request, res: Response) => {
   res.status(200).json({message: `Love, you got info for user ${req.params.id}`});
 };
 
@@ -35,7 +36,7 @@ export const getSingleUser = (req: Request, res: Response) => {
 //@route PUT /users/:id
 //@access public < Jungmee change this once you add authentication
 
-export const editUser = (req: Request, res: Response) => {
+export const editUser = asyncHandler(async (req: Request, res: Response) => {
   const {firstName, lastName, email} = req.body;
 
   if (!firstName && !lastName && !email) {
@@ -44,12 +45,12 @@ export const editUser = (req: Request, res: Response) => {
   }
 
   res.status(201).json({message: 'Love, you updated a user!'});
-};
+});
 
 //@desc delete 1 existing user
 //@route DELETE /users/:id
 //@access public < Jungmee change this once you add authentication
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({message: `Love, you deleted user ${req.params.id}`});
-};
+});
