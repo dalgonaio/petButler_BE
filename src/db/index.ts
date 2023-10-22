1cf5a589bee949f1b1c6c5f278b8e83715a6deb9
dalgonaio <jungmeevlee@gmail.com>
2023-10-21 20:39:54 -0400

    Tweak server file for heroku setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,25 +4,39 @@ dotenv.config();
 import {Pool, QueryConfig, QueryResult, QueryResultRow} from 'pg';
 const url = require('url');
 
-const params = url.parse(process.env.DATABASE_URL);
+const databaseUrl = process.env.DATABASE_URL;
 
-// Extract the individual components
-const auth = params.auth.split(':');
-const host = params.hostname;
-const port = params.port;
-const database = params.pathname.split('/')[1];
+if (!databaseUrl) {
+  console.error('DATABASE_URL environment variable is not set.');
+  process.exit(1); // Terminate the process in case of missing configuration
+}
 
-//Initial connection
-const pool = new Pool({
-  user: auth[0],
-  password: auth[1],
-  host: host,
-  port: port,
-  database: database,
-  ssl: {
-    rejectUnauthorized: false, // Only needed if your database requires SSL
-  },
-});
+let pool: Pool; // Define the type explicitly as Pool
+
+try {
+  const params = url.parse(databaseUrl);
+
+  // Extract the individual components
+  const auth = params.auth.split(':');
+  const host = params.hostname;
+  const port = params.port;
+  const database = params.pathname.split('/')[1];
+
+  // Initial connection
+  pool = new Pool({
+    user: auth[0],
+    password: auth[1],
+    host: host,
+    port: port,
+    database: database,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} catch (error) {
+  console.error('Error parsing DATABASE_URL:', error);
+  process.exit(1); // Terminate the process in case of an error
+}
 
 export const query = async <T extends QueryResultRow = any>(
   queryText: string,
@@ -33,6 +47,6 @@ export const query = async <T extends QueryResultRow = any>(
     values,
   };
 
-  const result = await pool.query<T>(queryConfig);
+  const result = await pool.query(queryConfig);
   return result;
 };
