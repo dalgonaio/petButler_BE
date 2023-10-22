3106559797e39ee066de074f37a50e65e17e408a
dalgonaio <jungmeevlee@gmail.com>
2023-10-21 21:18:58 -0400

    Update for heroku deploymnent

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,13 +3,30 @@ dotenv.config();
 
 import {Pool, QueryConfig, QueryResult, QueryResultRow} from 'pg';
 
+const url = require('url');
+
+//Params for both local/Heroku
+let auth;
+let host = process.env.DB_HOST;
+let port = parseInt(process.env.DB_PORT || '5432', 10);
+let database = process.env.DB_DATABASE;
+
+const db_URL = process.env.DATABASE_URL;
+if (db_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  auth = params.auth.split(':');
+  host = params.hostname;
+  port = params.port;
+  database = params.pathname.split('/')[1];
+}
+
 //Initial connection
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  database: process.env.DB_DATABASE,
+  user: auth[0],
+  password: auth[1],
+  host: host,
+  port: port,
+  database: database,
   ssl: {
     rejectUnauthorized: false,
   },
