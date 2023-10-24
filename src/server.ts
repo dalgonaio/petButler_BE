8631cbc0f1a4290b1bda640ef5129dcbf9052f63
dalgonaio <jungmeevlee@gmail.com>
2023-10-24 16:10:47 -0400

    Add Auth0 check that calls come from my frontend app

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,29 @@ import {NextFunction, Request, Response} from 'express';
 
 import express from 'express';
 import cors from 'cors';
+const {auth} = require('express-oauth2-jwt-bearer');
+
+//Components
 import userRoutes from './routes/userRoutes';
 import {errorHandler} from './middleware/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//AUTH
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: process.env.AUTH0_TOKEN,
+});
+
+//Lupin
+app.use(jwtCheck);
+
 //Middleware
 app.use(cors());
 app.use(express.json());
 
-//Jungmee for dev only
 // Custom middleware to log requests
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
   console.log(`Received a ${req.method} request at ${req.url}`);
