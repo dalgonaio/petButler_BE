c3da9daf84fc6b1c2cdb4d168b2cd3e4a8d73a65
dalgonaio <jungmeevlee@gmail.com>
2023-11-17 20:28:40 -0500

    change cors

--- src/server.ts
+++ src/server.ts
@@ -15,25 +15,22 @@ import {createUser} from './controllers/userController';
 const app = express();
 const port = process.env.PORT || 3000;
 
-const IP = 'http://localhost'
-var allowlist = [IP + ':3000', IP + ':3001', IP + ':3002', IP + ':3003']
-var corsOptionsDelegate = function (req:any, callback:any) {
+const IP = 'http://localhost';
+var allowlist = [IP + ':3000', IP + ':3001', IP + ':3002', IP + ':3003'];
+var corsOptionsDelegate = function (req: any, callback: any) {
   var corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    corsOptions = {origin: true}; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false } // disable CORS for this request
+    corsOptions = {origin: false}; // disable CORS for this request
   }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', "true");
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(cors(corsOptionsDelegate));
@@ -49,7 +46,7 @@ const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
 // Other middleware
 app.use(logMiddleware);
-app.get('/', (req, res) => res.status(200).send("hello from server3"))
+app.get('/', (req, res) => res.status(200).send('hello from server3'));
 //Auth0 Register users using an unprotected route as M2M rules on Auth0 is expensive
 app.post('/newAuth0User/', createUser);
 
@@ -61,7 +58,7 @@ import {query} from './db';
 app.use('/users/', userRoutes);
 app.get('/getUsers', (req, res) => {
   res.send('some users from database');
-})
+});
 
 //Error handler
 app.use(errorHandler);
