a58a6d069db5fbff7586de08b39385890e6d0bc7
dalgonaio <jungmeevlee@gmail.com>
2024-01-10 17:53:32 -0500

    Update get single day entries route to send back food name and total only

--- src/routes/trackerRoutes.ts
+++ src/routes/trackerRoutes.ts
@@ -9,7 +9,10 @@ import {
 } from '../controllers/trackerController';
 
 //Get all food diaries and add single food entry for a specific pet
-router.route('/:petId').get(getAllFoodEntries).get(getSingleDateEntries).post(addFoodEntry);
+router.route('/:petId').get(getAllFoodEntries).post(addFoodEntry);
+
+//Get all food diaries for a single pet on single date
+router.route('/:petId/:date').get(getSingleDateEntries);
 
 //Get, edit and delete single pet
 router.route('/:diaryId').put(editOneFoodEntry).delete(deleteFoodEntry);
